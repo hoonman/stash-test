4bf411ad625c22ab192afea5f281dafbde6ed77f
hoonman <jh020812@gmail.com>
2024-07-16 10:56:55 -0700

    t

--- testing.py
+++ testing.py
@@ -1,13 +1,13 @@
-def binary_search(arr, targe, high, low):
+def binary_search(arr, t, high, low):
     mid = (high + low) // 2
     if low > high:
         return False
-    elif arr[mid] == targe:
+    elif arr[mid] == t:
         return True
-    elif arr[mid] > targe:
-        return binary_search(arr, targe, mid - 1, low)
-    elif arr[mid] < targe:
-        return binary_search(arr, targe, high, mid + 1)
+    elif arr[mid] > t:
+        return binary_search(arr, t, mid - 1, low)
+    elif arr[mid] < t:
+        return binary_search(arr, t, high, mid + 1)
 
 arr = [1,2,3,4,5,6,7,8]
 print(binary_search(arr, 1, len(arr) - 1, 0))
