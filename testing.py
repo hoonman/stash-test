886a9015550f556d89d6c7bf43e86ee2bb2e32eb
hoonman <jh020812@gmail.com>
2024-07-16 10:43:59 -0700

    adding binary search script

---
+++ testing.py
@@ -0,0 +1,13 @@
+def binary_search(arr, target, high, low):
+    mid = (high + low) // 2
+    if low > high:
+        return False
+    elif arr[mid] == target:
+        return True
+    elif arr[mid] > target:
+        return binary_search(arr, target, mid - 1, low)
+    else:
+        return binary_search(arr, target, high, mid + 1)
+
+arr = [1,2,3,4,5,6,7,8]
+print(binary_search(arr, 1, len(arr) - 1, 0))
